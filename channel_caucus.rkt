6a1234a2c98de4ecb0672d9e045ba6a4338bd834
gamburgm <mitch.gamburg@gmail.com>
2020-04-08 17:36:27 -0400

    registries

--- channel_caucus.rkt
+++ channel_caucus.rkt
@@ -14,6 +14,51 @@
 (define (make-candidate name leader) 
   (thread (thunk (channel-put leader (candidate name)))))
 
+
+(define (make-candidate-registry . candidates voter-registry)
+  (define candidate-registry-channel (make-channel))
+  (define candidate-structs (map cdr candidtes))
+  (define candidate-chans   (map car candidtes))
+  (thread 
+    (thunk
+      (let loop ()
+        (sync
+          (handle-evt
+            candidate-registry-channel
+            (λ
+              (match
+                [(get-candidates recv-chan) 
+                 (channel-put recv-chan candidate-structs)])
+              (loop))))))))
+
+(define (make-voter-registry . voters)
+  (define voter-registry-channel (make-channel))
+  (define voter-structs (map cdr candidtes))
+  (define voter-chans   (map car candidtes))
+  ;; need to initialize all voters with the voter registry
+  (thread
+    (thunk
+      (let loop ()
+        (sync
+          (handle-evt
+            voter-registry-channel
+            (match-lambda
+              [(get-voters recv-chan)
+               (channel-put recv-chan voter-structs)]
+              [(round id candidates recv-chan)
+               (for ([voter-chan voter-chans])
+                 (channel-put voter-chan (round id candidates)))
+               (let vote-loop ([votes '()])
+                 (if (= (length votes) (length voters))
+                   (channel-put recv-chan (all-votes votes))
+                   (sync
+                     (handle-evt
+                       voter-registry-channel
+                       (match-lambda
+                         [(vote candidate-name)
+                          (vote-loop (cons candidate-name votes))])))))])))))))
+
+
 (define (make-voter name leader)
   (define voting-channel (make-channel))
   (thread (thunk (channel-put leader (voter name voting-channel)))))
@@ -41,6 +86,8 @@
   leader-channel)
 
 (define the-leader-channel (make-leader))
+
+
 (make-candidate "Bernie" the-leader-channel)
 (make-candidate "Biden" the-leader-channel)
 (make-candidate "Tulsi" the-leader-channel)
