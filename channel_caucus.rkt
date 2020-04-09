0b8af5dbee9268c4f4032465bbd5bbf80c5f00ab
gamburgm <mitch.gamburg@gmail.com>
2020-04-09 09:30:05 -0400

    on the right track

--- channel_caucus.rkt
+++ channel_caucus.rkt
@@ -4,38 +4,42 @@
 
 ;; a Name is a String
 
-(struct candidate (name) #:transparent)
+(struct candidate (name tax-rate) #:transparent)
 (struct voter (name vote-chan) #:transparent)
+(struct request-data (recv-chan) #:transparent)
+(struct round (id candidates recv-chan) #:transparent)
+(struct vote (name round-id candidate) #:transparent)
+(struct all-candidates (candidates) #:transparent)
+(struct all-voters (voters) #:transparent)
 
 ;; There is a presence-oriented conversation where candidates announce themselves to their voting leader
 
 ;; there is a presence-oriented conversation where voters announce themselves to their voting leader
 
-(define (make-candidate name leader) 
-  (thread (thunk (channel-put leader (candidate name)))))
+;; a little weird, but right now there's no need for this to do anything else, because of the candidate registry
+(define (make-candidate name tax-rate) (candidate name tax-rate))
 
-
-(define (make-candidate-registry . candidates voter-registry)
+(define (make-candidate-registry . candidate-structs)
   (define candidate-registry-channel (make-channel))
-  (define candidate-structs (map cdr candidtes))
-  (define candidate-chans   (map car candidtes))
   (thread 
     (thunk
       (let loop ()
         (sync
           (handle-evt
             candidate-registry-channel
-            (λ
-              (match
-                [(get-candidates recv-chan) 
-                 (channel-put recv-chan candidate-structs)])
-              (loop))))))))
+            (λ (evt)
+              (match evt
+                [(request-data recv-chan) 
+                 (channel-put recv-chan (all-candidates candidate-structs))])
+              (loop)))))))
+  candidate-registry-channel)
 
-(define (make-voter-registry . voters)
+(define (make-voter-registry candidate-registry . voters)
   (define voter-registry-channel (make-channel))
-  (define voter-structs (map cdr candidtes))
-  (define voter-chans   (map car candidtes))
-  ;; need to initialize all voters with the voter registry
+  (define voter-structs (map car voters))
+  (define voter-chans   (map cdr voters))
+  (for ([voter-chan voter-chans]) (channel-put voter-chan (registration-confirmation voter-registry-channel)))
+  (channel-put candidate-registry (request-data voter-registry-channel))
   (thread
     (thunk
       (let loop ()
@@ -43,56 +47,63 @@
           (handle-evt
             voter-registry-channel
             (match-lambda
-              [(get-voters recv-chan)
-               (channel-put recv-chan voter-structs)]
-              [(round id candidates recv-chan)
-               (for ([voter-chan voter-chans])
-                 (channel-put voter-chan (round id candidates)))
-               (let vote-loop ([votes '()])
-                 (if (= (length votes) (length voters))
-                   (channel-put recv-chan (all-votes votes))
-                   (sync
-                     (handle-evt
-                       voter-registry-channel
-                       (match-lambda
-                         [(vote candidate-name)
-                          (vote-loop (cons candidate-name votes))])))))])))))))
+              [(all-candidates candidate-structs) 
+               (for ([voter-chan voter-chans]) 
+                 (channel-put voter-chan (eligible-candidates candidate-structs)))
+               (loop)]
+              [(request-data recv-chan)
+               (channel-put recv-chan (all-voters voter-structs))
+               (loop)]))))))
+  voter-registry-channel)
+  
+(define (make-voter name)
+  (define voter-channel (make-channel))
+  (thread
+    (thunk
+      (let loop ([candidates '()]
+                 [voter-registry null])
+        (sync
+          (handle-evt
+            voter-channel
+            (match-lambda
+              [(registration-confirmation voter-registry-chan) (loop candidates voter-registry-chan)]
+              [(eligible-candidates candidate-structs) (loop candidate-structs voter-registry)])))))))
 
 
-(define (make-voter name leader)
-  (define voting-channel (make-channel))
-  (thread (thunk (channel-put leader (voter name voting-channel)))))
-
-(define (make-leader)
+(define (make-leader candidate-registry voter-registry)
   (define leader-channel (make-channel))
+  (channel-put candidate-registry (request-data leader-channel))
+  (channel-put voter-registry (request-data leader-channel))
   (thread
     (thunk
       (let loop ([candidates '()]
                  [voters '()])
-        (printf "we entered here\n")
         (sync
           (handle-evt
             leader-channel
             (match-lambda
-              ;; fix for tax-rate?
-              [(candidate name)
-               (printf "candidate made an announcement: ~a\n" name)
-               (printf "total candidates: ~a\n" candidates)
-               (loop (cons name candidates) voters)]
-              [(voter name vote-chan)
-               (printf "voter has registered: ~a\n" name)
-               (loop candidates (cons (voter name vote-chan) voters))]
-                ))))))
+              ;; endpoint: receiving candidates from candidate registry
+              [(all-candidates candidate-structs)
+               (printf "we got candidates! ~a\n" candidate-structs)
+               (loop candidate-structs voters)]
+              [(all-voters voter-structs)
+               (printf "we got voters! ~a\n" voter-structs)
+               (loop candidates voter-structs)]))))))
   leader-channel)
 
-(define the-leader-channel (make-leader))
 
+(define candidate-registry 
+  (make-candidate-registry
+    (make-candidate "Bernie" 35)
+    (make-candidate "Biden" 50)
+    (make-candidate "Tulsi" 2)))
 
-(make-candidate "Bernie" the-leader-channel)
-(make-candidate "Biden" the-leader-channel)
-(make-candidate "Tulsi" the-leader-channel)
+(define voter-registry 
+  (make-voter-registry 
+    candidate-registry
+    (make-voter "Mitch")
+    (make-voter "Sam")))
 
-(make-voter "Mitch" the-leader-channel)
-(make-voter "Sam" the-leader-channel)
+(define the-leader-channel (make-leader candidate-registry voter-registry))
 
 (sleep 1)
