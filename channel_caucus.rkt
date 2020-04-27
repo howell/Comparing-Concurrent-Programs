b09ddbf38d5f2ee871d6c5898760717708bd8a68
gamburgm <mitch.gamburg@gmail.com>
2020-04-27 12:30:27 -0400

    finished with candidate assumptions, moving to voters

--- channel_caucus.rkt
+++ channel_caucus.rkt
@@ -22,6 +22,8 @@
 ;; a DropOut is a (drop-out Name)
 (struct drop-out (name) #:transparent)
 
+(struct loser (name) #:transparent)
+
 ;; a Voter is a (voter Name Chan)
 (struct voter (name voting-chan) #:transparent)
 
@@ -47,7 +49,6 @@
 (struct all-voters (voters) #:transparent)
 
 ;;;; ASSUMPTIONS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;     a. Candidates also don't try to re-insert themselves after dropping out
 ;; 2. Voters don't try voting for multiple candidates
 ;; 3. Voters never try voting for candidates that are no longer available for voting
 ;; 4. Voters never leave early (with an announcement)
@@ -97,6 +98,7 @@
 (define (filter-candidates candidates blacklist)
   (filter (λ (cand) (not (set-member? blacklist (candidate-name cand)))) candidates))
 
+
 ;; Create the Candidate Registry thread and channel
 (define (make-candidate-registry)
   (define registration-chan (make-channel))
@@ -111,6 +113,7 @@
           (channel-put subscriber (all-candidates candidates))))
 
       (let loop ([candidates (set)]
+                 [all-cand-names (set)]
                  [subscribers (set)])
         (sync
           (handle-evt
@@ -118,18 +121,21 @@
             (match-lambda
               ;; a Candidate has registered!
               [(candidate name tax-rate results-chan) 
+               (when (set-member? all-cand-names name) (loop candidates all-cand-names subscribers))
                (define new-candidates (set-add candidates (candidate name tax-rate results-chan)))
+               (define new-names (set-add all-cand-names name))
                (update-subscribers subscribers new-candidates)
-               (loop new-candidates subscribers)]
+               (loop new-candidates new-names subscribers)]
               ;; a Candidate has dropped out!
               [(drop-out name)
                (define cand (for/first ([cand-struct (set->list candidates)]
                                        #:when (string=? name (candidate-name cand-struct)))
                                        cand-struct))
+               (when (not cand) (loop candidates all-cand-names subscribers))
                (define new-candidates (list->set (filter-candidates (set->list candidates) (set (candidate-name cand)))))
                (update-subscribers subscribers new-candidates)
                (log-caucus-evt "Candidate ~a has successfully dropped out of the race!" (candidate-name cand))
-               (loop new-candidates subscribers)]))
+               (loop new-candidates all-cand-names subscribers)]))
 
           (handle-evt
             receive-roll-chan
@@ -137,11 +143,11 @@
               ;; a channel has requested to be a Subscriber!
               [(subscribe chan)
                (channel-put chan (all-candidates candidates))
-               (loop candidates (set-add subscribers chan))]
+               (loop candidates all-cand-names (set-add subscribers chan))]
               ;; a channel has requested a snapshot of the current Candidates!
               [(request-msg chan)
                (channel-put chan (all-candidates candidates))
-               (loop candidates subscribers)]))))))
+               (loop candidates all-cand-names subscribers)]))))))
   (values registration-chan receive-roll-chan))
 
 ;; Create a Candidate thread
@@ -156,21 +162,6 @@
       (let loop ([in-the-race #t])
         (digest-results cand-struct threshold in-the-race registration-chan loop)))))
 
-;; receive and handle votes from latest election round
-;; Candidate Threshold Boolean Chan (Boolean -> void) -> void
-(define (digest-results cand threshold in-the-race reg-chan loop-func)
-  (define name (candidate-name cand))
-  (define results-chan (candidate-results-chan cand))
-
-  (define votes (tally-votes (channel-get results-chan)))
-  (cond
-    [(and in-the-race (< (hash-ref votes name 0) threshold))
-     (channel-put reg-chan (drop-out name))
-     (log-caucus-evt "Candidate ~a has submitted a request to drop out of the race!" name)
-     (loop-func #f)]
-    [else (loop-func #t)]))
-
-
 (define (make-stubborn-candidate name tax-rate threshold registration-chan)
   (define results-chan (make-channel))
   (define cand-struct (candidate name tax-rate results-chan))
@@ -180,10 +171,28 @@
       (channel-put registration-chan cand-struct)
       (let loop ([in-the-race #t])
         (when (not in-the-race) 
-          (channel-put registration-chan (candidate name tax-rate results-chan))
+          (channel-put registration-chan cand-struct)
           (log-caucus-evt "Stubborn Candidate ~a is trying to re-enter the race!" name)
           (loop #t))
         (digest-results cand-struct threshold in-the-race registration-chan loop)))))
+
+;; receive and handle votes from latest election round
+;; Candidate Threshold Boolean Chan (Boolean -> void) -> void
+(define (digest-results cand threshold in-the-race reg-chan loop-func)
+  (define name (candidate-name cand))
+  (define results-chan (candidate-results-chan cand))
+
+  (define msg (channel-get results-chan))
+  (match msg
+    [(tally votes)
+     (cond
+      [(and in-the-race (< (hash-ref votes name 0) threshold))
+       (channel-put reg-chan (drop-out name))
+       (log-caucus-evt "Candidate ~a has submitted a request to drop out of the race!" name)
+       (loop-func #f)]
+      [else (loop-func #t)])]
+    [(loser name)
+     (loop-func #f)]))
 
 ;; Create the Voter Registry thread and channel
 (define (make-voter-registry)
@@ -219,7 +228,7 @@
       (log-caucus-evt "Voter ~a is registering!" name)
       (channel-put candidate-registry (subscribe receive-candidates-chan))
       (channel-put voter-registry (voter name voting-chan))
-      (let loop ([candidates (set)]) ;; candidates just gets 're-assigned' by calling loop with a different argument, this is just to clarify type
+      (let loop ([candidates (set)])
         (sync
           (handle-evt
             receive-candidates-chan
@@ -239,6 +248,16 @@
                (channel-put leader-chan (vote name voting-for))
                (loop candidates)])))))))
 
+(define (make-greedy-voter name rank-candidates voter-registry candidate-registry)
+  (define receive-candidates-chan (make-channel))
+  (define voting-chan (make-channel))
+  (thread
+    (thunk
+      (log-caucus-evt "Greedy voter ~a is registering!" name)
+      (channel-put candidate-registry (subscribe receive-candidates-chan))
+      (channel-put voter-registry (voter name voting-chan))
+      (let loop ([candidates (set)])
+
 ;; Make the Vote Leader thread
 (define (make-vote-leader candidate-registry voter-registry)
   (define retrieve-candidates-chan (make-channel))
@@ -252,6 +271,7 @@
 
       ;; TODO ASSUMPTION: there are always both voters and candidates.
 
+
       ;; Start a sequence of votes to determine an elected candidate
       ;; (Setof Name) -> Candidate
       (define (run-caucus removed-candidates)
@@ -261,6 +281,17 @@
         (channel-put voter-registry (request-msg retrieve-voters-chan))
 
         (define STATES '(SETUP VOTING))
+
+        ;; Determine if the vote leader is ready to transition to the next state
+        ;; (Setof Voter) (Setof Candidate) -> STATE
+        (define (next-state voters candidates)
+          (if (and (not (set-empty? voters)) (not (set-empty? candidates))) 'VOTING 'SETUP))
+
+        ;; Transition (loop) on the next state for the vote leader
+        ;; (Setof Voter) (Setof Candidate) (V C -> C) -> C
+        (define (transition-states voters candidates loop)
+          (loop voters candidates (next-state voters candidates)))
+
         (let loop ([curr-state 'SETUP]
                    [voters (set)]
                    [candidates (set)]) ;; this is just to demonstrate the type
@@ -271,19 +302,14 @@
                  retrieve-voters-chan
                  (match-lambda
                    [(all-voters new-voters) 
-                    (if (not (set-empty? candidates))
-                      (loop 'VOTING new-voters candidates)
-                      (loop 'SETUP new-voters candidates))]))
+                    (transition-states new-voters candidates loop)]))
                (handle-evt
                  retrieve-candidates-chan
                  (match-lambda
                    [(all-candidates new-candidates)
                     (define eligible-candidates (list->set (filter-candidates (set->list new-candidates) removed-candidates)))
-                    (if (not (set-empty? voters))
-                      (loop 'VOTING voters eligible-candidates)
-                      (loop 'SETUP voters eligible-candidates))])))]
+                    (transition-states voters eligible-candidates loop)])))]
             ['VOTING
-             ;; TODO poorly formatted
              (for ([voter (set->list voters)])
                (channel-put (voter-voting-chan voter) 
                             (request-vote 
@@ -313,11 +339,15 @@
                        (log-caucus-evt "Candidate ~a has been elected!" (candidate-name front-runner))
                        front-runner]
                       [else
-                        (for ([cand-struct (set->list candidates)]) (channel-put (candidate-results-chan cand-struct) (tally new-votes)))
-                        (define loser (argmin (λ (cand) (hash-ref new-votes (candidate-name cand) 0)) (set->list candidates)))
-                        (log-caucus-evt "Candidate ~a has been eliminated from the race!" (candidate-name loser))
-                        (define next-candidates (set-remove candidates loser))
-                        (run-caucus (set-add removed-candidates (candidate-name loser)))])]
+                        (define losing-cand (argmin (λ (cand) (hash-ref new-votes (candidate-name cand) 0)) (set->list candidates)))
+                        (for ([cand-struct (set->list candidates)]) 
+                          (channel-put (candidate-results-chan cand-struct) (tally new-votes)))
+                        (for/first ([cand-struct (set->list candidates)]
+                                    #:when (string=? (candidate-name losing-cand) (candidate-name cand-struct)))
+                                   (channel-put (candidate-results-chan cand-struct) (loser (candidate-name cand-struct))))
+                        (log-caucus-evt "Candidate ~a has been eliminated from the race!" (candidate-name losing-cand))
+                        (define next-candidates (set-remove candidates losing-cand))
+                        (run-caucus (set-add removed-candidates (candidate-name losing-cand)))])]
                    [else (voting-loop new-votes)])])))))
         
       (define winner (run-caucus (set)))
@@ -361,20 +391,7 @@
        (sort candidates compare-names)
        cand-names)))
 
-
-
-     #|(for ([cand-name (reverse cand-names)])
-       (define candidate? (findf (λ (cand) (string=? cand-name (candidate-name cand))) candidates))
-       (if candidate?
-         (cons candidate? (sort (remove candidate? candidates) compare-names))
-         (sort candidates compare-names)))))|#
-
 ;;;;;;;;;;;; EXECUTION ;;;;;;;;;;;;
-
-;; (define c (make-channel))
-;; (printf "res: ~a\n" ((stupid-sort "A" "B") (list (candidate "X" 0 c) (candidate "Y" 0 c) (candidate "A" 0 c) (candidate "Z" 0 c) (candidate "B" 0 c))))
-
-
 
 (define-values (candidate-registration candidate-roll) (make-candidate-registry))
 (define-values (voter-registration voter-roll) (make-voter-registry))
@@ -384,10 +401,12 @@
 (make-candidate "Tulsi" 6 0 candidate-registration)
 (make-candidate "Donkey" 1000000000000000 200 candidate-registration)
 (make-candidate "Vermin Supreme" 35 0 candidate-registration)
-(make-candidate "1" 0 0 candidate-registration)
-(make-candidate "2" 0 0 candidate-registration)
-(make-candidate "3" 0 0 candidate-registration)
-(make-candidate "aaaaa" 0 100 candidate-registration)
+(make-candidate "Vermin Supreme 2" 35 0 candidate-registration)
+(make-candidate "Vermin Supreme 3" 35 0 candidate-registration)
+(make-stubborn-candidate "1" 0 2000 candidate-registration)
+;; (make-candidate "2" 0 0 candidate-registration)
+;; (make-candidate "3" 0 0 candidate-registration)
+;; (make-candidate "aaaaa" 0 100 candidate-registration)
 
 (make-voter "ABC" (stupid-sort "Bernie") voter-registration candidate-roll)
 (make-voter "DEF" (stupid-sort "Bernie") voter-registration candidate-roll)
@@ -412,8 +431,10 @@
 (make-voter "223" (stupid-sort "Donkey") voter-registration candidate-roll)
 (make-voter "334" (stupid-sort "Donkey") voter-registration candidate-roll)
 (make-voter "445" (stupid-sort "Donkey") voter-registration candidate-roll)
-#|
-(make-voter "098" (stupid-sort "1" "Tulsi") voter-registration candidate-roll)
-|#
+(make-voter "446" (stupid-sort "1") voter-registration candidate-roll)
+;; (make-voter "447" (stupid-sort "1") voter-registration candidate-roll)
+;; (make-voter "448" (stupid-sort "1") voter-registration candidate-roll)
+;; (make-voter "449" (stupid-sort "1") voter-registration candidate-roll)
+;; (make-voter "450" (stupid-sort "1") voter-registration candidate-roll)
 
 (thread-wait (make-vote-leader candidate-roll voter-roll))
