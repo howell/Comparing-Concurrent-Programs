348c6665199f6c56fbc3a47d4ada8c003b2b7741
gamburgm <mitch.gamburg@gmail.com>
2020-05-21 13:40:42 -0400

    works

--- caucus.exs
+++ caucus.exs
@@ -199,7 +199,7 @@ defmodule VoteLeader do
         IO.puts "And the winner is: #{frontrunner}!"
       else
         {loser, _} = Enum.min(tally, fn {_, count1}, {_, count2} -> count1 <= count2 end)
-        Enum.each(candidates, fn %Candidate{name: _, tax_rate: _, pid: pid} -> send pid, tally end)
+        Enum.each(candidates, fn %Candidate{name: _, tax_rate: _, pid: pid} -> send pid, {:ballot, tally} end)
         IO.puts "Our loser is #{loser}!"
 
         setup_voting(voters, MapSet.put(blacklist, cand_names[loser]), cand_registry)
@@ -243,9 +243,15 @@ end
 
 cand_registry = CandidateRegistry.spawn
 
-Candidate.spawn("Bernie", 50, 10000, cand_registry)
-Candidate.spawn("Biden", 25, 10000, cand_registry)
-Candidate.spawn("Tulsi", 10, 10000, cand_registry)
+Candidate.spawn("Bernie", 50, 0, cand_registry)
+Candidate.spawn("Biden", 25, 0, cand_registry)
+Candidate.spawn("Tulsi", 10, 0, cand_registry)
+Candidate.spawn("1", 10, 100000, cand_registry)
+Candidate.spawn("2", 10, 0, cand_registry)
+Candidate.spawn("3", 10, 0, cand_registry)
+Candidate.spawn("4", 10, 0, cand_registry)
+Candidate.spawn("5", 10, 0, cand_registry)
+Candidate.spawn("6", 10, 0, cand_registry)
 
 voter_registry = VoterRegistry.spawn
 
@@ -253,12 +259,27 @@ Voter.spawn("ABC", voter_registry, cand_registry, StupidSort.generate("Tulsi"))
 Voter.spawn("DEF", voter_registry, cand_registry, StupidSort.generate("Tulsi"))
 Voter.spawn("GHI", voter_registry, cand_registry, StupidSort.generate("Tulsi"))
 Voter.spawn("JKL", voter_registry, cand_registry, StupidSort.generate("Tulsi"))
+
 Voter.spawn("123", voter_registry, cand_registry, StupidSort.generate("Biden"))
 Voter.spawn("124", voter_registry, cand_registry, StupidSort.generate("Biden"))
 Voter.spawn("125", voter_registry, cand_registry, StupidSort.generate("Biden"))
-Voter.spawn("118", voter_registry, cand_registry, StupidSort.generate("Bernie"))
-Voter.spawn("108", voter_registry, cand_registry, StupidSort.generate("Bernie"))
-Voter.spawn("107", voter_registry, cand_registry, StupidSort.generate("Bernie"))
+Voter.spawn("CBA", voter_registry, cand_registry, StupidSort.generate("Biden"))
+Voter.spawn("FED", voter_registry, cand_registry, StupidSort.generate("Biden"))
+Voter.spawn("IHG", voter_registry, cand_registry, StupidSort.generate("Biden"))
+
+Voter.spawn("118", voter_registry, cand_registry, StupidSort.generate("1"))
+Voter.spawn("108", voter_registry, cand_registry, StupidSort.generate("2"))
+Voter.spawn("107", voter_registry, cand_registry, StupidSort.generate("3"))
+Voter.spawn("151", voter_registry, cand_registry, StupidSort.generate("4"))
+Voter.spawn("169", voter_registry, cand_registry, StupidSort.generate("5"))
+Voter.spawn("186", voter_registry, cand_registry, StupidSort.generate("6"))
+Voter.spawn("207", voter_registry, cand_registry, StupidSort.generate("2"))
+Voter.spawn("230", voter_registry, cand_registry, StupidSort.generate("1"))
+Voter.spawn("A", voter_registry, cand_registry, StupidSort.generate("2"))
+Voter.spawn("B", voter_registry, cand_registry, StupidSort.generate("2"))
+Voter.spawn("C", voter_registry, cand_registry, StupidSort.generate("2"))
+Voter.spawn("D", voter_registry, cand_registry, StupidSort.generate("2"))
+Voter.spawn("E", voter_registry, cand_registry, StupidSort.generate("2"))
 
 pid = VoteLeader.spawn(voter_registry, cand_registry)
 ref = Process.monitor(pid)
