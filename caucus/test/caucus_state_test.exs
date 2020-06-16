90f2a3bfb1733b405fe6b35105e55ab8ae18212f
gamburgm <mitch.gamburg@gmail.com>
2020-06-15 21:47:03 -0400

    corrected property test

--- caucus/test/caucus_state_test.exs
+++ caucus/test/caucus_state_test.exs
@@ -9,34 +9,26 @@ defmodule RegistryStateProps do
     end
   end
 
-  defmodule CandidateState do
-    def initial_state do
-      generator = char_list()
-      IO.puts inspect(generator)
-      name = generator
-      threshold = 5
+  def initial_state do
+    let {name, threshold} <- {char_list(), nat()} do
       %{pid: Candidate.spawn(name, any(), threshold, self()), name: name, threshold: threshold}
     end
-
-    def command(%{pid: pid, name: name, threshold: num}) do
-      {:call, CandidateInterface, :ballot, [pid, %{name => 6}]}
-    end
-
-    def command({:call, CandidateInterface, :ballot, [arg1 | [arg2 | []]]}) do
-      CandidateInterface.ballot(arg1, arg2)
-    end
-
-    def command(_), do: true
-
-    def precondition(_, _), do: true
-    def postcondition(_, _, _), do: true
-
-    def next_state(s, _, _), do: s
   end
 
+  def command(%{pid: pid, name: name, threshold: num}) do
+    let votes <- range(num + 1, :inf) do
+      {:call, CandidateInterface, :ballot, [pid, %{name => votes}]}
+    end
+  end
+
+  def precondition(_, _), do: true
+  def postcondition(_, _, _), do: true
+
+  def next_state(s, _, _), do: s
+
   property "Will not withdraw from race if gets enough votes" do
-    forall cmds <- commands(CandidateState) do
-      {_, _, res} = run_commands(CandidateState, cmds)
+    forall cmds <- commands(__MODULE__) do
+      {_, _, res} = run_commands(__MODULE__, cmds)
       res == :ok
     end
   end
