deed93de0d1bd292de281dcb76f78de9a3092145
gamburgm <mitch.gamburg@gmail.com>
2020-06-18 21:17:58 -0400

    f

--- caucus/test/caucus_state_test.exs
+++ caucus/test/caucus_state_test.exs
@@ -67,19 +67,25 @@ defmodule RegistryState do
     end
   end
 
-  def next_state(s, _, {:call, _, :send_value, struct}) do
+  def next_state(s, _, {:call, _, :send_value, [_, struct]}) do
     Map.put(s, :data, MapSet.put(s.data, struct))
   end
 
-  def next_state(s, _, {:call, _, :remove_value, struct}) do
+  def next_state(s, _, {:call, _, :remove_value, [_, struct]}) do
     Map.put(s, :data, MapSet.delete(s.data, struct))
   end
 
-  def precondition(s, {:call, _, :remove_value, struct}) do
+  def precondition(s, {:call, _, :remove_value, [_, struct]}) do
     MapSet.member?(s.data, struct)
   end
 
   def precondition(_, _), do: true
+
+  def postcondition(%{data: data}, {:call, _, :send_value, [_, struct]}, _) do
+    IO.puts inspect(struct)
+    IO.puts inspect(data)
+    MapSet.member?(data, struct)
+  end
 
   def postcondition(%{pid: pid}, _, _) do
     Process.alive?(pid)
