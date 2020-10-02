72ca12359a911317dc14a359f2f1e528c9509c99
gamburgm <mitch.gamburg@gmail.com>
2020-10-02 12:24:43 -0400

    separate documentation and structs from code

---
+++ caucus_struct.rkt
@@ -0,0 +1,70 @@
+#lang syndicate/actor
+
+(provide voter vote round candidate candidate-name tally elected winner)
+
+;; a Name is a (caucus-unique) String
+
+;; an ID is a unique Symbol
+
+;; a TaxRate is a Number
+
+;; a Threshold is a Number
+
+;; a VoteCount is a Number
+
+;; a Region is a (caucus-unique) String
+
+;; a Voter is a (voter Name Region)
+(assertion-struct voter (name region))
+
+;; a Vote is a (voter Name ID Region Name), where the first name is the voter and the
+;; second is who they are voting for
+(assertion-struct vote (voter round region candidate))
+
+;; a Round is a (round ID Region (Listof Name))
+(assertion-struct round (id region candidates))
+
+;; a Candidate is a (candidate Name TaxRate Threshold)
+(assertion-struct candidate (name tax-rate))
+
+;; a Tally is a (tally Name Region VoteCount)
+(assertion-struct tally (name region vote-count))
+
+;; an Elected is a (elected Name Region)
+(assertion-struct elected (name region))
+
+;; a Winner is a (winner Name)
+(assertion-struct winner (name))
+
+;; There are four actor roles:
+;; - Caucus Leaders
+;; - Candidates
+;; - Voters
+;; - Region Manager
+
+;; There are two presence-oriented conversations:
+;; Voters announce their presence through a Voter assertion
+;; Candidates announce their presence through a Candidate assertion
+
+;; There is a conversation about voting:
+
+;; The Caucus Leader initiates a round of voting by making a Round assertion
+;; with a new ID and the list of candidates still in the running. Voters vote in
+;; a certain round by making a Vote assertion with the corresponding round ID,
+;; their name, and the name of the candidate they are voting for.
+
+;; There is an election results conversation, where the Caucus Leader announces
+;; the winner with an Elected assertion
+
+;; There is a conversation about the winner for a region. Each region is identified by
+;; a name that voters explicitly register for. When a candidate is elected by a caucus,
+;; they announce the election of that candidate and alert the region manager, who then
+;; closes voting and declares a final winner when one of the candidates has received 
+;; a plurality of the votes.
+
+;; There are multiple bad actors.
+;; - Stubborn Candidate: a candidate who tries to re-enter the race after having been dropped --> could be implemented differently than the way I have it now
+;; - Greedy Voter: A voter that tries voting twice when possible.
+;; - Stubborn Voter: A voter that always votes for the same candidate, even if that candidate isn't eligible.
+;; - Late-Joining Voter: A voter who joins voting late (i.e. isn't available to vote for the first round).
+;; - Unregistered Voter: A voter who votes without being registered to vote.
--- channel_caucus.rkt
+++ channel_caucus.rkt
@@ -1,5 +1,6 @@
 #lang racket
 (require racket/set)
+(require "channel_struct.rkt")
 
 (provide make-abstract-registry make-candidate make-stubborn-candidate make-voter make-greedy-voter make-stubborn-voter make-sleepy-voter make-vote-leader make-region-manager stupid-sort)
 
@@ -9,108 +10,6 @@
 ;; NOTE requires using `info@caucus` as the log-level when program is executed
 (define (log-caucus-evt evt . vals)
   (log-message caucus-log 'info (logger-name caucus-log) (apply format evt vals)))
-
-;; a Name is a string
-
-;; a Tax-Rate is a number
-
-;; a Threshold is a number
-
-;; a Chan is a channel
-
-;; a Region is a string
-
-;; a Candidate is a (candidate Name Tax-Rate Chan)
-(struct candidate (name tax-rate results-chan) #:transparent)
-
-;; a DropOut is a (drop-out Name)
-(struct drop-out (name) #:transparent)
-
-;; a Loser is a (loser Name)
-(struct loser (name) #:transparent)
-
-;; a Voter is a (voter Name Region Chan)
-(struct voter (name region voting-chan) #:transparent)
-
-;; a Request-Msg is a (request-msg Chan)
-(struct request-msg (chan) #:transparent)
-
-;; a RequestVoters is a (request-voters Region Chan)
-(struct request-voters (region leader-chan) #:transparent)
-
-;; a Request-Vote is a (request-vote Chan)
-(struct request-vote (candidates chan) #:transparent)
-
-;; a Vote is a (vote Name Name)
-(struct vote (name candidate) #:transparent)
-
-;; a ballot-results is a (ballot-results (Hashof Name . number))
-(struct ballot-results (votes) #:transparent)
-
-;; an All-Candidates is a (all-candidates [Setof Candidate])
-(struct all-candidates (candidates) #:transparent)
-
-;; an All-Voters is a (all-voters [Setof Voter])
-(struct all-voters (voters) #:transparent)
-
-;; a DeclareLeader is a (declare-leader Region Chan)
-(struct declare-leader (region manager-comm-chan) #:transparent)
-
-;; a DeclareManager is a (declare-manager Chan)
-(struct declare-manager (results-chan) #:transparent)
-
-;; a DeclareWinner is a (declare-winner Name)
-(struct declare-winner (candidate) #:transparent)
-
-;;;;; REGISTRY STRUCTS ;;;;;
-
-;; a Publish is a (publish Any)
-(struct publish (val) #:transparent)
-
-;; a Withdraw is a (withdraw Any)
-(struct withdraw (val) #:transparent)
-
-;; a Subscribe is a (subscribe Chan)
-(struct subscribe (subscriber-chan) #:transparent)
-
-;; a Message is a (message Chan)
-(struct message (response-chan) #:transparent)
-
-;; a Payload is a (payload [Setof Any])
-(struct payload (data) #:transparent)
-
-;;;; ENTITIES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 1. Candidates        
-;; 2. Candidate Registry
-;; 3. Voters            
-;; 4. Voter Registry    
-;; 5. Vote Leader       
-;; 6. Region Manager
-;; 
-
-;;;; CONVERSATIONS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Publish Conversations
-;; 1. Candidates publish their information to the Candidate Registry through a `candidate` struct sent to the Registry's channel
-;; 2. Voters publish their information to the Voter Registry through a `voter` struct sent to the Registry's channel
-;; 3. Candidates can remove themselves from eligibility by sending a `drop-out` struct to the Candidate Registry
-;;     -> This occurs when the candidate receives a number of votes below the candidate's threshold for staying in the race.
-;; 4. Vote Leaders publish their information to the Region Manager through a `declare-leader` struct
-;;
-;; Subscribe Conversations
-;; 1. Voters subscribe to the Candidate Registry to receive the most up-to-date list of available Candidates whenever a candidate registers.
-;; 2. The Vote Leader subscribes to the Candidate Registry to receive the same information that voters do.
-;; 
-;; Message Conversations
-;; 1. The Vote Leader sends a `request-msg` struct to the Voter Registry to receive the most up-to-date list of current voters.
-;; 2. The Vote Leader sends a `request-msg` struct to the Candidate Registry to receive the most up-to-date list of available candidates.
-;;
-;; Voting Conversations
-;; 1. The Voting Leader sends every Voter (through their `voter` struct) a request to vote through the `request-vote` struct, sending a List of valid candidate names.
-;; 2. Voters reply by picking a name to vote for and sending the Vote Leader a `vote` struct.
-;; 3. If one candidate has received a majority of votes, then that candidate is elected. If not, the least voted for candidate is removed, and voting begins again.
-;; 4. At the end of every round of voting, the ballot-results of votes is sent to every Candidate.
-;; 5. When a candidate wins an election, the Vote Leader publishes that information to the Region Manager. When all caucuses have reported, the majority winner is elected.
-;;
 
 ;;;; HELPERS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
