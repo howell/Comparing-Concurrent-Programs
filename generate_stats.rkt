872a7a08a006cd1606b9ef7dbc5e4c33025fe8c7
Mitch Gamburg <mitch.gamburg@gmail.com>
2020-11-16 13:08:12 -0500

    don't select largest component

--- generate_stats.rkt
+++ generate_stats.rkt
@@ -18,11 +18,21 @@
       (if (<= dist 0) 0 dist))
     (/ (* (length diffs) (- (length diffs) 1)) 2)))
 
-(define (calculate-stats input-file)
-  (define diffs (parse-diff-file input-file))
-
+(define (print-stats diffs)
   (printf "number of changes: ~a\n" (change-count diffs))
   (printf "average size of change: ~a\n" (average-change diffs))
   (printf "average distance between changes: ~a\n" (average-dist diffs)))
+
+(define (largest-diff diffs)
+  (for/fold ([curr-diff (first diffs)])
+            ([new-diff (rest diffs)])
+    (if (> (diff-meta-size new-diff) (diff-meta-size curr-diff))
+      new-diff
+      curr-diff)))
+
+(define (calculate-stats input-file)
+  (define diffs (parse-diff-file input-file))
+  (define diffs-without-component (remove (largest-diff diffs) diffs))
+  (print-stats diffs-without-component))
 
 (calculate-stats "input.txt")
