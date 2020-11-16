bec3f3557039e1646425d8b52accd3c1387e54fa
Mitch Gamburg <mitch.gamburg@gmail.com>
2020-11-16 13:55:31 -0500

    modify graph to align

--- generate_graph.rkt
+++ generate_graph.rkt
@@ -25,32 +25,34 @@
   (define tick-posns (for/list ([tick (in-range 0 x-axis-length 50)]) tick))
   (ticks (tick-layout tick-posns) tick-formatter))
 
+(define (create-y-ticks y-axis-length)
+  (define tick-posns (for/list ([tick (in-range 0 y-axis-length 20)]) tick))
+  (ticks (tick-layout tick-posns) tick-formatter))
+
 ;; NOTE string cannot be empty
 (define (capitalize-string str)
   (string-append (string (char-upcase (string-ref str 0))) (substring str 1)))
 
-(define (plot-changes lang input-file program)
-  (define file-size (length (file->lines program)))
-  (define approx-size (* 50 (ceiling (/ file-size 50))))
+(define (plot-changes lang input-file program x-max y-max)
   (define output-file (format "~a.jpeg" lang))
   
   (define graph-title (format "~a Diff Visualized" (capitalize-string lang)))
 
   (define change-rectangles
     (for/list ([change (in-list (parse-diff-file input-file))])
-      (define start (diff-line change))
-      (define size (diff-size change))
+      (define start (diff-meta-line change))
+      (define size (diff-meta-size change))
       (vector (ivl start (+ start 1)) (ivl 0 size))))
 
   (parameterize ([plot-x-tick-label-anchor 'top-right]
                  [plot-x-tick-label-angle 90]
-                 [plot-width approx-size]
-                 [plot-x-ticks (create-x-ticks file-size)]
+                 [plot-x-ticks (create-x-ticks x-max)]
+                 [plot-y-ticks (create-y-ticks y-max)] ;; should be max size of largest component change across the programs
                  [plot-x-far-ticks no-ticks]
                  [plot-y-far-ticks no-ticks])
-    (plot-file (rectangles change-rectangles) output-file #:title graph-title #:x-label "Line of code" #:y-label "Number of lines inserted" #:x-min 1 #:x-max (+ file-size 1))))
+    (plot-file (rectangles change-rectangles) output-file #:title graph-title #:x-label "Line of code" #:y-label "Number of lines inserted" #:x-min 1 #:x-max x-max #:y-max y-max)))
 
 (define-values (lang new-file)
   (command-line #:args (lang new-file) (values lang new-file)))
 
-(plot-changes lang "input.txt" new-file)
+(plot-changes lang "input.txt" new-file 650 100)
