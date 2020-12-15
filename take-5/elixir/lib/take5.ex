9a8d9bd1130c19b7b06cdf9027a1589a678cbc45
Mitch Gamburg <mitch.gamburg@gmail.com>
2020-12-15 14:27:23 -0500

    new dealer structure

--- take-5/elixir/lib/take5.ex
+++ take-5/elixir/lib/take5.ex
@@ -32,126 +32,190 @@
 # of the Players with the lowest scores in the game.
 
 # the Dealer that administers and manages the game for Players
-defmodule Dealer do
-  # [List-of PlayerInfo] [PID of DeclaredWinners] -> PID
-  def spawn(players, game_results_pid) do
-    if length(players) < 2 or length(players) > 10 do
-      raise "Take-5 is played with 2-10 players"
-    else
-      spawn fn -> initialize_game(players, game_results_pid) end
+
+defmodule RoundHandler do
+  use GenServer
+
+  # Client API
+
+  def start_link(round_no, players) do
+    GenServer.start_link(__MODULE__, [round_no, players], [])
+  end
+
+  # Server API
+
+  def init([round_no, players]) do
+    start_timeout()
+    {:ok, %{round_no: round_no, players: players, moves: []}}
+  end
+
+  def handle_info(
+    m = {:move, round_no, _player, _c},
+    state = %{round_no: round_no, players: players, moves: moves}
+  ) do
+    Logging.log_move(m)
+    new_moves = [m | moves]
+    if length(new_moves) == length(players) do
+      conclude_round(state)
     end
+  end
+
+  def handle_info(:timeout, state) do
+    conclude_round(state)
+  end
+
+  defp start_timeout do
+    Process.send_after self(), :timeout, 1000
+  end
+
+  defp conclude_round(%{round_no: n, players: _p, moves: m}) do
+    Take5.Dealer.end_round(n, m)
+    # NOTE I think this works
+    {:stop, :normal}
+  end
+end
+
+defmodule GameState do
+  defstruct [:hands, :rows, :scores]
+end
+
+defmodule Dealer do
+  use GenServer
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
   end
   
-  # [List-of PlayerInfo] [PID of DeclaredWinners] -> void
-  defp initialize_game(players, game_results_pid) do
-    starting_deck = Deck.create_deck()
-    player_names = Enum.map(players, fn {:player, name, _} -> name end)
-    {:ok, starting_hands, new_deck} = Deck.deal(starting_deck, player_names)
-
-    # Elixir ranges are inclusive on both ends
-    {starting_rows, _} = Enum.reduce(1..4, {[], new_deck}, fn _, {curr_rows, curr_deck} ->
-      {:ok, drawn_card, new_deck} = Deck.draw_one(curr_deck)
-      {[{:row, [drawn_card]} | curr_rows], new_deck}
-    end)
-
-    play_game(players, starting_hands, starting_rows, Map.new(player_names, fn n -> {n, 0} end), game_results_pid)
+  def init(:ok) do
+    open_registration()
+    {:ok, %{reg_closed: false, players: [], curr_round: 0, game_state: %{}}}
   end
 
-  # [List-of PlayerInfo] [Hash-of PlayerID [List-of Card]] [List-of Row] Scores [PID of DeclaredWinners] -> void
-  defp play_game(players, hands, rows, scores, game_results_pid) do
-    Logging.log_rows(rows)
-    start_round(1, players, hands, rows, scores, game_results_pid)
+  def register(server, player) do
+    GenServer.call(server, {:register, player})
   end
 
-  # Nat [List-of PlayerInfo] [Hash-of PlayerID [List-of Card]] [List-of Row] Scores [PID of DeclaredWinners] -> void
-  defp start_round(round_no, players, hands, rows, scores, game_results_pid) do
-    for {:player, name, pid} <- players do
-      send pid, {:round, round_no, Map.get(hands, name), rows, self()}
-    end
-    # Send a timeout signal after one second
-    Process.send_after self(), :timeout, 1000
-
-    play_round([], round_no, players, hands, rows, scores, game_results_pid)
+  def end_round(server, round_no, moves) do
+    GenServer.call(server, {:end_round, round_no, moves})
   end
 
-  # [List-of Move] Nat [List-of PlayerInfo] [Hash-of PlayerID [List-of Card]] [List-of Row] Scores [PID of DeclaredWinners] -> void
-  defp play_round(moves, round_no, players, hands, rows, scores, game_results_pid) do
-    # Produce new hash containing entries with valid keys
-    # [Hash-of A Any] [Set-of A] -> [Hash-of A Any]
-    filter_keys = fn old_hash, valid_keys ->
-      Enum.reduce(old_hash, %{}, fn {key, val}, new_hash ->
-        if MapSet.member?(valid_keys, key) do
-          Map.put(new_hash, key, val)
-        else
-          new_hash
-        end
-      end)
-    end
+  # Server API
 
-    receive do
-      :timeout ->
-        valid_players = MapSet.new(Enum.map(moves, fn {:move, _, name, _} -> name end))
-        new_hands = filter_keys.(hands, valid_players)
-        new_scores = filter_keys.(scores, valid_players)
-
-        conclude_round(
-          moves,
-          round_no,
-          MapSet.to_list(valid_players),
-          new_hands,
-          rows,
-          new_scores,
-          game_results_pid
-        )
-      m = {:move, ^round_no, _name, _c} ->
-        Logging.log_move(m)
-        new_moves = [m | moves]
-        if length(new_moves) == length(players) do
-          conclude_round(new_moves, round_no, players, hands, rows, scores, game_results_pid)
-        else
-          play_round(new_moves, round_no, players, hands, rows, scores, game_results_pid)
-        end
+  # TODO add game state here?
+  def handle_call({:register, p}, s = %{reg_closed: r, players: players, curr_round: round_no, game_state: g}) do
+    if r do
+      {:reply, {:error, :closed}, s}
+    else
+      {:reply, :ok, %{reg_closed: r, players: [p | players], curr_round: round_no, game_state: g}}
     end
   end
 
-  # [List-of Move] Nat [List-of PlayerInfo] [Hash-of PlayerID [List-of Card]] [List-of Row] Scores [PID of DeclaredWinners] -> void
-  defp conclude_round(moves, round_no, players, hands, rows, scores, game_results_pid) do
-    {new_rows, new_scores} = Rules.play_round(rows, moves, scores)
+  # TODO should this be a `cast` instead of a `call`?
+  def handle_call(
+    {:end_round, round_no, moves},
+    %{round_no: round_no, reg_closed: true, players: players, game_state: game_state}
+  ) do
+    # TODO play_round helper function?
+    %GameState{hands: hands, rows: rows, scores: scores} = game_state
+
+    moved_player_names = MapSet.new(Enum.map(moves, fn {:move, _no, name, _c} -> name end))
+    players_that_moved = Enum.filter(players, fn {:player, name, _} -> MapSet.member?(moved_player_names, name) end)
+
+    filtered_scores = filter_keys(scores, moved_player_names)
+
+    {new_rows, new_scores} = Rules.play_round(rows, moves, filtered_scores)
+
     Logging.log_rows(new_rows)
     Logging.log_scores(new_scores)
 
     if round_no == 10 do
       winner_s = Rules.lowest_scores(new_scores)
       Logging.log_winners(winner_s)
-      send game_results_pid, {:declared_winners, winner_s}
+      IO.puts "Winners! #{inspect winner_s}"
+      {:stop, :normal}
     else
       new_hands = Enum.reduce(moves, %{}, fn {:move, _no, name, c}, new_hands ->
         Map.put(new_hands, name, List.delete(Map.get(hands, name), c))
       end)
 
-      start_round(round_no + 1, players, new_hands, new_rows, new_scores, game_results_pid)
+      new_round_no = round_no + 1
+
+      new_game_state = %GameState{hands: new_hands, rows: new_rows, scores: new_scores}
+      start_round(new_round_no, players_that_moved, new_game_state)
+      {:reply, :ok, %{round_no: new_round_no, players: players_that_moved, reg_closed: true, game_state: new_game_state}}
     end
+  end
+
+
+  def handle_info(:start_game, %{players: players}) do
+    game_state = initialize_game(players)
+    start_round(1, players, game_state)
+    {:noreply, %{players: players, reg_closed: true, curr_round: 1, game_state: game_state}}
+  end
+
+  # For junk messages
+  def handle_info(_msg, state) do
+    {:noreply, state}
+  end
+
+  ## Helper Functions
+
+  defp open_registration do
+    Process.send_after self(), :start_game, 20_000
+  end
+
+  defp initialize_game(players) do
+    starting_deck = Deck.create_deck()
+    player_names = Enum.map(players, fn {:player, name, _} -> name end)
+
+    {:ok, starting_hands, new_deck} = Deck.deal(starting_deck, player_names)
+
+    {starting_rows, _} = Enum.reduce(1..4, {[], new_deck}, fn _, {curr_rows, curr_deck} ->
+      {:ok, drawn_card, new_deck} = Deck.draw_one(curr_deck)
+      {[{:row, [drawn_card]} | curr_rows], new_deck}
+    end)
+
+    %GameState{hands: starting_hands, rows: starting_rows, scores: Map.new(player_names, fn n -> {n, 0} end)}
+  end
+
+  defp start_round(round_no, players, %GameState{hands: h, rows: rows, scores: _}) do
+    {:ok, round_pid} = RoundHandler.start_link(round_no, players)
+
+    Enum.each(players, fn {:player, name, pid} ->
+      player_hand = Map.get(h, name)
+      send pid, {:round, round_no, player_hand, rows, round_pid}
+    end)
+  end
+
+  defp filter_keys(hash, keys) do
+    Enum.reduce(hash, %{}, fn {key, val}, new_hash ->
+      if MapSet.member?(keys, key) do
+        Map.put(new_hash, key, val)
+      else
+        new_hash
+      end
+    end)
   end
 end
 
 defmodule Player do
-  def spawn(client, dealer) do
+  def create(client) do
     spawn fn ->
-      name = register_player(client, dealer)
-      loop(name, client, dealer)
+      name = register_player(client)
+      loop(name, client)
     end
   end
 
-  defp register_player(client, dealer) do
+  defp register_player(client) do
     {:ok, msg} = :gen_tcp.recv(client, 0)
     case Translator.parse(Poison.decode!(msg)) do
       player = {:player, name} ->
-        send dealer, player
+        send Take5.Dealer, player
         name
     end
   end
 
-  defp loop(name, client, dealer) do
+  defp loop(name, client) do
     receive do
       {:round, round_no, cards, rows} ->
         move_request = {:move_request, round_no, cards, rows}
@@ -160,8 +224,8 @@ defmodule Player do
         {:ok, msg} = :gen_tcp.recv(client, 0)
         case Translator.parse(Poison.decode!(msg)) do
           m = {:move, ^round_no, ^name, _c} ->
-            send dealer, m
-            loop(name, client, dealer)
+            send Take5.Dealer, m
+            loop(name, client)
         end
     end
 
@@ -169,18 +233,51 @@ defmodule Player do
 end
 
 defmodule PlayerServer do
-  def spawn(port, dealer) do
+  def spawn(port) do
     spawn fn ->
+      # FIXME What're these options for?
       {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
-      loop(socket, dealer)
+      loop(socket)
     end
   end
 
-  defp loop(socket, dealer) do
+  defp loop(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
-    pid = Player.spawn(client, dealer)
+    
+
+    # FIXME if we're doing things this way, probably don't need a spawn in player
+    {:ok, pid} = Task.Supervisor.start_child(Player.TaskSupervisor, fn -> Player.spawn(client, dealer) end)
     :ok = :gen_tcp.controlling_process(client, pid)
     loop(socket, dealer)
   end
 end
 
+# defmodule Take5.Supervisor do
+#   use Supervisor
+
+#   def start_link(opts) do
+#     Supervisor.start_link(__MODULE__, :ok, opts)
+#   end
+
+#   def init(:ok) do
+#     children = [
+#       PlayerServer
+#     ]
+
+#     Supervisor.init(children, strategy: :one_for_one)
+#   end
+# end
+
+defmodule Take5 do
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      {Dealer, name: Take5.Dealer},
+      {PlayerServer, [8900]}
+    ]
+
+    opts = [strategy: :one_for_one, name: Take5.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
--- take-5/elixir/main.exs
+++
@@ -1,6 +0,0 @@
-Dealer.spawn(Player.create_players([:a, :b, :c, :d, :e], RandomPlay), self())
-
-receive do
-  {:declared_winners, winners} -> IO.puts "Winners! #{inspect winners}"
-end
-
