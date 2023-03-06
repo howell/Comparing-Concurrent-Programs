fb102ac3b474457bf47850d32d7e567844feacf6
Sam Caldwell <samc@ccs.neu.edu>
2023-03-06 16:21:10 -0500

    fixup and experiment w typed caucus

--- typed-caucus.rkt
+++ typed-caucus.rkt
@@ -1,7 +1,7 @@
-#lang typed/syndicate/roles
+#lang typed/syndicate
 
-(require (except-in typed/drivers/timestate activate!)
-         (prefix-in timestate: (only-in typed/drivers/timestate activate!)))
+(require (except-in typed/syndicate/drivers/timestate activate!)
+         (prefix-in timestate: (only-in typed/syndicate/drivers/timestate activate!)))
 
 (define-type-alias Name String)
 (define-type-alias ID Symbol)
@@ -75,22 +75,24 @@
 
 (define (spawn-candidate [name Name] [tax-rate TaxRate] [threshold Threshold])
   (spawn CandComms
+    (lift+define-role candidate-impl
     (start-facet cand
       (printf "Candidate ~a has entered the race!\n" name)
       (assert (candidate name tax-rate))
       (on (message (tally name $region $vote-count))
           (when (< vote-count threshold)
-            (stop cand))))))
+            (stop cand)))))))
 
 
 ;; Name TaxRate Threshold -> Candidate
 (define (spawn-stubborn-candidate [name Name] [tax-rate TaxRate] [threshold Threshold])
   (spawn (U CandComms (Observe Candidate))
+    (lift+define-role stubborn-candidate-impl
     (start-facet be-stubborn
       (printf "Candidate ~a is going to be stubborn!\n" name)
       (on start (spawn-candidate name tax-rate threshold))
       (on (retracted (candidate name tax-rate))
-          (spawn-candidate name tax-rate threshold)))))
+          (spawn-candidate name tax-rate threshold))))))
 
 ;; Find the name of the highest ranked candidate
 (define (ranked-vote [candidates (Set Candidate)] [round-candidates (List Name)] [rank-candidates (→fn (List Candidate) (List Candidate))]
@@ -109,13 +111,14 @@
                         [name Name]
                         [region Region]
                         [register? Bool])
+    (lift+define-role voter-impl
     (start-facet _
       (printf "Voter ~a is intending to register in region ~a!\n" name region)
       (define/query-set candidates (candidate $name:Name $tr:TaxRate) (candidate name tr))
       (when register? (assert (voter name region)))
 
       (during (round $id:ID region ($ round-candidates (List Name)))
-        (voting-procedure id region round-candidates (ref candidates)))))
+        (voting-procedure id region round-candidates (ref candidates))))))
 
 (define (voter-skeleton [voting-procedure (proc ID Region (List Name) (Set Candidate) → ★/t #:endpoints ((Shares Vote)))]
                         [name Name]
@@ -148,7 +151,7 @@
 (define (spawn-leaving-voter [name Name] [region Region] [rank-candidates (→fn (List Candidate) (List Candidate))] [round-limit Int])
   (spawn VoterComms
     (start-facet count-rounds
-      (field [round-count 0])
+      (field [round-count : Int 0])
 
       (define (voting-procedure [id ID] [region Region] [round-candidates (List Name)] [candidates (Set Candidate)])
         (set! round-count (add1 (ref round-count)))
@@ -164,8 +167,8 @@
 (define (spawn-late-joining-voter [name Name] [region Region] [rank-candidates (→fn (List Candidate) (List Candidate))] [round-limit Int])
   (spawn VoterComms
     (start-facet count-rounds
-      (field [round-count 0]
-             [registered? #f])
+      (field [round-count : Int 0]
+             [registered? : Bool #f])
 
       (begin/dataflow
         (when (ref registered?)
@@ -198,6 +201,7 @@
 ;; Region -> Leader
 (define (spawn-leader [region Region])
   (spawn LeaderComms
+    (lift+define-role leader-impl
     (start-facet vote-leader
       (printf "The Vote Leader for region ~a has joined the event!\n" region)
       (define/query-set voters (voter $name region) name)
@@ -239,18 +243,8 @@
                     (printf "Voter ~a has voted for candidate ~a in ~a in region ~a!\n" who for round-id region)
                     (set! voter-to-candidate (hash-set (ref voter-to-candidate) who for))])))
 
-          (on start
-            (start-facet wait
-              (define one-sec-from-now (get-one-second-from-now))
-
-              (on (asserted (later-than one-sec-from-now))
-                  (printf "Timeout reached on this round!\n")
-                  (define new-voters (for/set ([voter (ref valid-voters)]
-                                               #:when (hash-has-key? (ref voter-to-candidate) voter))
-                                       voter))
-                  (set! valid-voters new-voters))))
-
-          (begin/dataflow
+          ;;(begin/dataflow
+          (define (count!)
             (define votes
               (for/fold ([votes (Hash Name Int) (hash)])
                         ([(voter cand) (ref voter-to-candidate)])
@@ -280,18 +274,32 @@
                                         (set->list (ref still-in-the-running))))
                   (printf "The front-runner for ~a in region ~a is ~a! The loser is ~a!\n" round-id region front-runner loser)
                   (define next-candidates (set-intersect (ref candidates) (set-remove (ref still-in-the-running) loser)))
-                  (stop run-a-round (realize! (start-round next-candidates (ref valid-voters))))])))))
+                  (stop run-a-round (realize! (start-round next-candidates (ref valid-voters))))])))
+
+          (on start
+              (start-facet wait
+                (define one-sec-from-now (get-one-second-from-now))
+
+                (on (asserted (later-than one-sec-from-now))
+                    (printf "Timeout reached on this round!\n")
+                    (define new-voters (for/set ([voter (ref valid-voters)]
+                                                 #:when (hash-has-key? (ref voter-to-candidate) voter))
+                                         voter))
+                    (set! valid-voters new-voters)
+                    (count!))))
+
+))
 
       (on (realize (start-round ($ candidates (Set Name)) ($ voters (Set Name))))
           (run-round candidates voters))
 
       (define one-second-from-now (get-one-second-from-now))
       (on start
-        (start-facet wait
+        (start-facet signup-wait
           (on (asserted (later-than one-second-from-now))
               ;; ASSUME: at least one candidate and voter at this point
               (printf "The race has begun in region ~a!\n" region)
-              (stop wait (realize! (start-round (ref candidates) (ref voters))))))))))
+              (stop signup-wait (realize! (start-round (ref candidates) (ref voters)))))))))))
 
 ;; Name -> [[Listof Candidate] -> [Listof Candidate]]
 (define (stupid-sort [cand-name Name] -> (→fn (List Candidate) (List Candidate)))
@@ -308,6 +316,7 @@
 ;; -> Manager
 (define (spawn-manager [regions (List Region)])
   (spawn ManagerComms
+    (lift+define-role manager-impl
     (start-facet manager
       (field [caucus-results (Hash Name Int) (hash)])
       (printf "Manager online\n")
@@ -320,7 +329,7 @@
           (when (= num-results (length regions))
             (match-define (tuple (some $winning-candidate) _)
               (for/fold ([best-cand (Maybe Name) none]
-                         [their-votes -1])
+                         [their-votes : Int -1])
                         ([(cand-name cand-votes) (ref caucus-results)])
                 (if (< cand-votes their-votes)
                     (tuple best-cand their-votes)
@@ -328,7 +337,7 @@
             (stop manager
              (start-facet announce
               (printf "The winner of the election is ~a!\n" winning-candidate)
-              (assert (outbound (winner winning-candidate))))))))))
+              (assert (outbound (winner winning-candidate)))))))))))
 
 #|
 
@@ -458,6 +467,19 @@
          (spawn-candidate "Tulsi" 16 300)
          (spawn-stubborn-candidate "Donkey" 0 1000)))))
 
+(module+ main
 (run-ground-dataspace τc
                       (timestate:activate!)
-                      (startup))
+                      (startup)))
+
+(module+ test
+  #;(verify-actors TT manager-impl)
+  (define-type-alias TimerMock
+    (Role (timer-driver)
+          (During (Observe (LaterThanT Int))
+                  (Shares (LaterThanT Int)))))
+  (check-deadlock-free manager-impl
+                       voter-impl
+                       candidate-impl
+                       TimerMock)
+  )
